7658dfea89b5e7ef4eb8daa7dc23c65e39180233
Ian Anderson <ian@gozumi.com>
2012-12-19 22:22:28 +0000

    This commit contains a version of the continuous timeline that is working to a great extent. the only problem is that when the back button is clicked, the new month markers do not appear correctly.

--- calendar.js
+++ calendar.js
@@ -4,46 +4,59 @@ if(!Array.prototype.last) {
 	};
 }
 
-var containerWidth = window.innerWidth;
-	containerHeight = 600;
-	width = window.innerWidth,
-	height = 400,
-	cellWidth = Math.round(width / 54),
-	cellHeight = Math.round(height / 7),
-	originX = 0,
-	shiftSize = cellWidth * 30;
+//---------- CONSTANTS
+
+var ONE_DAY=1000*60*60*24;
+
+
+//---------- Date formatters
+
 var day = d3.time.format("%w"),
 	week = d3.time.format("%U"),
 	monthText = d3.time.format('%B'),
 	month = d3.time.format('%m'),
 	year = d3.time.format('%Y'),
 	percent = d3.format(".1%"),
-	format = d3.time.format("%Y-%m-%d"),
-	fromYear = 2011,
-	toYear = 2013;
+	format = d3.time.format("%Y-%m-%d");
 
-var xFisheye = d3.fisheye.scale(d3.scale.identity).domain([0, width]).focus(width/2),
-	yFisheye = d3.fisheye.scale(d3.scale.identity).domain([0, height]).focus(90);
+//---------- Private Variables
+
+var _containerWidth = window.innerWidth;
+	_containerHeight = 600;
+	_calendarWidth = window.innerWidth,
+	_calendarHeight = 400,
+	dateRange = _getDateRange(new Date());
+
+
+
+var	cellWidth = Math.round(_calendarWidth / dateRange.weekCount),
+	cellHeight = Math.round(_calendarHeight / 7);
+	// originX = 0,
+	// shiftSize = cellWidth * 30;
+	// fromYear = 2011,
+	// toYear = 2013;
+
+var xFisheye = d3.fisheye.scale(d3.scale.identity).domain([0, _calendarWidth]).focus(_calendarWidth/2);
+//	yFisheye = d3.fisheye.scale(d3.scale.identity).domain([0, height]).focus(90);
 var color = d3.scale.quantize().domain([-0.05, 0.05]).range(d3.range(11).map(function(d) {
 	return "q" + d + "-11";
 }));
 
 
-var ONE_DAY=1000*60*60*24;
 
-var focusDate = new Date();
+//var focusDate = new Date();
 
-var startWeek = week(getDateRange(focusDate).days[0]);
+//var startWeek = week(getDateRange(focusDate).days[0]);
 
 
-var svg = d3.select("body").append("svg").attr("width", containerWidth).attr("height", containerHeight).attr("class", "RdYlGn").append("g").attr("transform", "translate(0,60)");
+var svg = d3.select("body").append("svg").attr("width", _containerWidth).attr("height", _containerHeight).attr("class", "RdYlGn").append("g").attr("transform", "translate(0,60)");
 
-var dayGroup = svg.selectAll(".day").data(getDateRange(focusDate).days, keyFunctionDay).enter().append('g').attr('class', 'day');
+var dayGroup = svg.selectAll(".day").data(dateRange.days, keyFunctionDay).enter().append('g').attr('class', 'day');
 
 dayGroup.append("path").attr("d", dayPath).attr("class", "dayRect").append("title").text(function(d) {
 	return format(d);
 });
-dayGroup.append("path").attr("d", monthPath).attr("class", "monthLine");
+d3.selectAll('g.day').append("path").attr("d", monthPath).attr("class", "monthLine");
 // }).datum(format);
 
 // var months = svg.selectAll(".month").data(getDateRange(focusDate).months, keyFunctionMonth).enter().append("g").attr("class", "month").append('path').attr('class', 'monthBound').attr('d', monthPath);
@@ -64,7 +77,7 @@ dayGroup.append("path").attr("d", monthPath).attr("class", "monthLine");
 // });
 
 function dayPath(d) {
-	var startX = ( (week(d)-startWeek) * cellWidth ) + weekOffset(d) + originX;
+	var startX = dateRange.weekNumber(d) * cellWidth;
 	var startY = day(d) * cellHeight;
 
 	var x1 = xFisheye(startX);
@@ -85,7 +98,7 @@ function dayPath(d) {
 }
 
 function monthPath(d) {
-	var x1 = ( (week(d)-startWeek) * cellWidth ) + weekOffset(d) + originX;
+	var x1 = dateRange.weekNumber(d) * cellWidth;
 	var y1 =  (day(d) * cellHeight) + cellHeight;
 	var y2 = y1 - cellHeight;
 	var x2 = x1 + cellWidth;
@@ -125,62 +138,37 @@ function weekOffset(d) {
 }
 
 d3.select("div.forward").on("click", function() {
-	focusDate = new Date(focusDate.getTime()+(30*ONE_DAY));
+	var focusDate = new Date(dateRange.focusDate.getTime()+(30*ONE_DAY));
+	dateRange = _getDateRange(focusDate);
 
-	var days = svg.selectAll(".day").data(getDateRange(focusDate).days, keyFunctionDay);
+	var days = svg.selectAll(".day").data(dateRange.days, keyFunctionDay);
 	days.exit().remove();
 	var dayGroups = days.enter().append('g').attr('class', 'day');
 	dayGroups.append("path").attr("d", dayPath).attr("class", "dayRect").append("title").text(function(d) {
 		return format(d);
 	});
 
-	dayGroups.append("path").attr("d", monthPath).attr("class", "monthLine");
+	var monthGroups = days.enter().append("path").attr("d", monthPath).attr("class", "monthLine");
 	
-	//originX = originX - (width / 12);
-
-	svg.selectAll(".dayRect").transition().duration(400).attr('d', dayPath);
-//	svg.selectAll(".monthLine").transition().duration(400).attr('d', monthPath);
-
+	svg.selectAll("path.dayRect").transition().duration(400).attr('d', dayPath);
+	svg.selectAll("path.monthLine").transition().duration(400).attr('d', monthPath);
 });
 
 d3.select("div.backward").on("click", function() {
-	// focusDate = new Date(focusDate.getTime()+(30*ONE_DAY));
-	// var dateRange = getDateRange(focusDate);
+	var focusDate = new Date(dateRange.focusDate.getTime()-(30*ONE_DAY));
+	dateRange = _getDateRange(focusDate);
 
-	// // you need to do the selection again to achieve	
-	// //days.data(dateRange.days, keyFunction).enter().append("path").attr("d", dayPath).attr("class", "day").append("title").text(function(d) {
-	// var days = svg.selectAll('path.day').data(dateRange.days, keyFunctionDay);
-	// days.enter().append("path").attr("d", dayPath).attr('class', 'day').append("title").text(function(d) {
-	// 	return format(d);
-	// });
-	// var months2 = svg.selectAll(".month").data(dateRange.months, keyFunctionMonth);
-	// months2.enter().append("g").attr("class", "month").append('path').attr('class', 'monthBound').attr('d', monthPath);
+	var days = svg.selectAll(".day").data(dateRange.days, keyFunctionDay);
+	days.exit().remove();
+	var dayGroups = days.enter().append('g').attr('class', 'day');
+	dayGroups.append("path").attr("d", dayPath).attr("class", "dayRect").append("title").text(function(d) {
+		return format(d);
+	});
 
-	// originX = originX + (width / 12);
-
-	// days.transition().duration(500).attr('d', dayPath);
-
-	// //var months2 = svg.selectAll(".month").data(dateRange.months);.enter().append("g").attr("class", "month").append('path').attr('class', 'monthBound').attr('d', monthPath);
-
-	// months.transition().duration(500).attr('d', monthPath);
-	// monthLabels.transition().duration(500).attr('x', function(d) {
-	// 	return xFisheye(((week(d) - startWeek) * cellWidth) + weekOffset(d) + cellWidth + 5 + originX);
-	// });
-
-
-
-	// focusDate = new Date(focusDate.getTime()+(30*ONE_DAY));
-	// var dateRange = getDateRange(focusDate);
-
-	// originX = originX + (width/12);
-
-	// var days2 = svg.selectAll('path.day').data(dateRange.days, keyFunctionDay);
-
-	// days2.transition().duration(500).attr('d', dayPath);
-	// months.transition().duration(500).attr('d', monthPath);
-	// monthLabels.transition().duration(500).attr('x', function(d) {
-	// 	return xFisheye(((week(d) - startWeek) * cellWidth) + weekOffset(d) + cellWidth + 5 + originX);
-	// });
+	var monthGroups = days.enter().append("path").attr("d", monthPath).attr("class", "monthLine");
+	
+	svg.selectAll("path.dayRect").transition().duration(400).attr('d', dayPath);
+	svg.selectAll("path.monthLine").transition().duration(400).attr('d', monthPath);
 });
 
 // d3.csv("dji.csv", function(error, csv) {
@@ -208,14 +196,66 @@ function keyFunctionMonth(d) {
 	return monthText(d) + ' ' + year(d);
 }
 
-function getDateRange(targetDate) {
+function _getDateRange(targetDate) {
 	var fromDate = new Date(targetDate.getTime() - (ONE_DAY * 180));
 	fromDate = new Date(fromDate.getFullYear(), fromDate.getMonth(), 1);
 	var toDate = new Date(targetDate.getTime() + (ONE_DAY * 180));
 	toDate = new Date(toDate.getFullYear(), toDate.getMonth() + 1, 0);
 	toDate = new Date(toDate.getTime() + ONE_DAY);
 
-	return {days: d3.time.days(fromDate, toDate), months: d3.time.months(fromDate, toDate)};
+	var days = d3.time.days(fromDate, toDate);
+	var months = d3.time.months(fromDate, toDate);
+	var weekCount = Math.floor(days.length/7);
+	if ((days.length % 7) > 0) {
+		weekCount++;
+	}
+	if (day(days.last()) < day(days[0])) {
+		weekCount++;
+	}
+
+	var rangeObject = {
+		focusDate: targetDate,
+		days: days,
+		months: months,
+		weekCount: weekCount,
+		weekNumber: function(date) {
+			var justTheDatePart = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+			var weekNumber;
+			for(var i = 0; i < this.days.length; i++) {
+				if(justTheDatePart.getTime() == this.days[i].getTime()) {
+					weekNumber = Math.floor(i / 7);
+					if (day(days[i]) < day(days[0])) {
+						weekNumber++;
+					}
+					return weekNumber;
+				}
+			}
+			return weekNumber;
+		}
+	};
+
+	return rangeObject;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
