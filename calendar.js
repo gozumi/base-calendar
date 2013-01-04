d020acca163b035f5eecb3e32a30b58bc0fca7ca
Ian Anderson <ian@gozumi.com>
2013-01-04 15:11:55 +0000

    The forward and backward movement are now working smoothly. The next step is to modularise the movement functionality.

--- calendar.js
+++ calendar.js
@@ -7,9 +7,9 @@ if(!Array.prototype.last) {
 
 //---------- CONSTANTS
 var ONE_DAY = 1000 * 60 * 60 * 24,
-	SHIFT_DAYS = 30,
 	DURATION = 500,
 	WEEKS_TO_DISPLAY = 14;
+	SHIFT_DAYS = Math.floor(WEEKS_TO_DISPLAY / 2) * 7,
 	TODAY = new Date(); TODAY.setHours(0,0,0,0);
 
 //---------- Date formatters
@@ -43,34 +43,10 @@ var svg = d3.select("body").append("svg").attr('id', 'gozumi-calendar').attr("wi
 
 //------------ Initialisation funtion
 (function init() {
-	svg.selectAll('g.day').data(dateRange.days, keyFunctionDay).enter().append('g').attr('class', 'day').append("path").attr("d", dayPath).attr("class", function(d){
-		var classString;
-		if ((d.getMonth() % 2)) {
-			classString = "dayRect odd";
-
-		} else {
-			classString = "dayRect even";
-		}
-		if (d.getTime() == TODAY.getTime()) {
-			classString = 'dayRect today';
-		}
-		return classString;
-	}).append("title").text(function(d) {
+	svg.selectAll('g.day').data(dateRange.days, keyFunctionDay).enter().append('g').attr('class', 'day').append("path").attr("d", dayPath).attr("class", dayRectClass).append("title").text(function(d) {
 		return format(d);
 	});
-	svg.selectAll("g.day").append('text').attr('class', function(d){
-		var classString;
-		if ((d.getMonth() % 2)) {
-			classString = "date odd";
-
-		} else {
-			classString = "date even";
-		}
-		if (d.getTime() == TODAY.getTime()) {
-			classString = 'date today';
-		}
-		return classString;
-	}).text(function(d){
+	svg.selectAll("g.day").append('text').attr('class', dayTextClass).text(function(d){
 		return d.getDate();
 	}).attr('x', function(d){
 		return xFisheye((dateRange.weekNumber(d) * cellWidth) + 4);
@@ -107,34 +83,47 @@ function dayPath(d) {
 	return pathString;
 }
 
+function dayRectClass(d) {
+	var classString;
+	if ((d.getMonth() % 2)) {
+		classString = "dayRect odd";
+
+	} else {
+		classString = "dayRect even";
+	}
+	if (d.getTime() == TODAY.getTime()) {
+		classString = 'dayRect today';
+	}
+	return classString;
+}
+
+function dayTextClass(d) {
+	var classString;
+	if ((d.getMonth() % 2)) {
+		classString = "date odd";
+
+	} else {
+		classString = "date even";
+	}
+	if (d.getTime() == TODAY.getTime()) {
+		classString = 'date today';
+	}
+	return classString;
+}
 
 //---------------------- Forward event handler
 d3.select("div.forward").on("click", function() {
 	var firstOfNewDays = new Date(dateRange.days.last().getTime() + ONE_DAY);
-	var lastOfNewDays = new Date(firstOfNewDays.getTime() + (ONE_DAY * 40));
+	var lastOfNewDays = new Date(firstOfNewDays.getTime() + (ONE_DAY * SHIFT_DAYS));
 	dateRange.days = [].concat(dateRange.days, d3.time.days(firstOfNewDays, lastOfNewDays));
 	dateRange.months = [].concat(dateRange.months, d3.time.months(firstOfNewDays, lastOfNewDays));
 
 	// Draw new days and months out of view so that they slide in nicely
 	var days = svg.selectAll('g.day').data(dateRange.days, keyFunctionDay).enter().append('g');
-	days.attr('class', 'day').append("path").attr("d", dayPath).attr("class", function(d){
-		if ((d.getMonth() % 2)) {
-			return "dayRect odd";
-
-		} else {
-			return "dayRect even";
-		}
-	}).append("title").text(function(d) {
+	days.attr('class', 'day').append("path").attr("d", dayPath).attr("class", dayRectClass).append("title").text(function(d) {
 		return format(d);
 	});
-	days.append('text').attr('class', function(d){
-		if ((d.getMonth() % 2)) {
-			return "date odd";
-
-		} else {
-			return "date even";
-		}
-	}).text(function(d){
+	days.append('text').attr('class', dayTextClass).text(function(d){
 		return d.getDate();
 	}).attr('x', function(d){
 		return xFisheye((dateRange.weekNumber(d) * cellWidth) + 4);
@@ -170,30 +159,16 @@ d3.select("div.forward").on("click", function() {
 //---------------------- Backward event handler
 d3.select("div.backward").on("click", function() {
 	var lastOfNewDays = new Date(dateRange.days[0].getTime());
-	var firstOfNewDays = new Date(lastOfNewDays.getTime() - (ONE_DAY * 40));
+	var firstOfNewDays = new Date(lastOfNewDays.getTime() - (ONE_DAY * SHIFT_DAYS) - ONE_DAY);
 	var interimDays = [].concat(d3.time.days(firstOfNewDays, lastOfNewDays), dateRange.days);
 	var interimMonths = [].concat(d3.time.months(firstOfNewDays, lastOfNewDays), dateRange.months);
 
 	// Draw new days and months out of view so that they slide in nicely
 	var days = svg.selectAll('g.day').data(interimDays, keyFunctionDay).enter().append('g');
-	days.attr('class', 'day').append("path").attr("d", dayPath).attr("class", function(d){
-		if ((d.getMonth() % 2)) {
-			return "dayRect odd";
-
-		} else {
-			return "dayRect even";
-		}
-	}).append("title").text(function(d) {
+	days.attr('class', 'day').append("path").attr("d", dayPath).attr("class", dayRectClass).append("title").text(function(d) {
 		return format(d);
 	});
-	days.append('text').attr('class', function(d){
-		if ((d.getMonth() % 2)) {
-			return "date odd";
-
-		} else {
-			return "date even";
-		}
-	}).text(function(d){
+	days.append('text').attr('class', dayTextClass).text(function(d){
 		return d.getDate();
 	}).attr('x', function(d){
 		return xFisheye((dateRange.weekNumber(d) * cellWidth) + 4);
@@ -201,7 +176,7 @@ d3.select("div.backward").on("click", function() {
 		return (day(d) * cellHeight) + 14;
 	});
 
-	svg.selectAll('text.monthLabel').data(interimMonths, keyFunctionMonth).enter().append('text').attr('class', 'monthLabel').attr('y', -10).text(function(d){
+	svg.selectAll('text.monthLabel').data(dateRange.months, keyFunctionMonth).enter().append('text').attr('class', 'monthLabel').attr('y', -10).text(function(d){
 		return monthText(d) + ' ' + d.getFullYear();
 	}).attr('x', function(d){
 		return xFisheye((dateRange.weekNumber(d) * cellWidth) + cellWidth + (cellWidth/2));
@@ -212,18 +187,17 @@ d3.select("div.backward").on("click", function() {
 	dateRange = _getDateRange(focusDate);
 
 	// Shift the DOM elements based on the re-focused range
-	svg.selectAll(".dayRect").data(dateRange.days, keyFunctionDay).transition().duration(DURATION).attr("d", dayPath);
-	svg.selectAll('text.date').data(dateRange.days, keyFunctionDay).transition().duration(DURATION).attr('x', function(d){
+	svg.selectAll('path.dayRect').transition().duration(DURATION).attr("d", dayPath);
+	svg.selectAll('text.date').transition().duration(DURATION).attr('x', function(d){
 		return xFisheye((dateRange.weekNumber(d) * cellWidth) + 4);
 	});
-	svg.selectAll('text.monthLabel').data(dateRange.months, keyFunctionMonth).transition().duration(DURATION).attr('x', function(d){
+	svg.selectAll('text.monthLabel').transition().duration(DURATION).attr('x', function(d){
 		return xFisheye((dateRange.weekNumber(d) * cellWidth) + cellWidth + (cellWidth/2));
 	});
 
 	// Remove DOM elements that are out of range
-	svg.selectAll('g.day').data(dateRange.days, keyFunctionDay).exit().remove();
-	svg.selectAll('text.date').data(dateRange.days, keyFunctionDay).exit().remove();
-	svg.selectAll('text.monthLabel').data(dateRange.months, keyFunctionMonth).exit().remove();
+	svg.selectAll('g.day').data(dateRange.days, keyFunctionDay).exit().transition().duration(DURATION).remove();
+	svg.selectAll('text.monthLabel').data(dateRange.months, keyFunctionMonth).exit().transition().duration(DURATION).remove();
 });
 
 function keyFunctionDay(d) {
@@ -256,6 +230,11 @@ function _getDateRange(targetDate) {
 	var days = d3.time.days(fromDate, toDate);
 	var months = d3.time.months(fromDate, toDate);
 
+	if (months[0].getMonth() !== fromDate.getMonth()) {
+		fromDate.setDate(1);
+		months.unshift(fromDate);
+	}
+
 	var rangeObject = {
 		focusDate: targetDate,
 		days: days,
@@ -273,9 +252,9 @@ function _getDateRange(targetDate) {
 			} else {
 				dayNumber = d3.time.days(date, this.days[0]).length * -1;
 				weekNumber = Math.floor(dayNumber / 7);
-				if(day(days[0]) < day(date)) {
-					weekNumber++;
-				}
+				// if(day(days[0]) < day(date)) {
+				// 	weekNumber++;
+				// }
 			}
 			return weekNumber;
 		}
